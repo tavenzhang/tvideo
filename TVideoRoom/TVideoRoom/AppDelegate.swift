ab6c473e3691d68feb1dadf8aee62210b5730bbc
tavenzhang <258333601@163.com>
2016-10-07 13:56:11 +0800

    new11

--- TVideoRoom/TVideoRoom/AppDelegate.swift
+++ TVideoRoom/TVideoRoom/AppDelegate.swift
@@ -17,6 +17,7 @@ import Crashlytics
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
+    
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
